109be2e29c23fcb809b54cd91213cf2d1d084bdf
ipratikner <pratiknerlikar@gmail.com>
2021-10-17 00:11:36 +0530

    Added: welcome msg added

---
+++ src/com/bridgelabz/LineCompare.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class LineCompare {
+
+    public static void main(String[] args) {
+        System.out.println("***Welcome To The Line Compare Computation Problem***");
+    }
+}
